6f8d14cdad411fa5b1c5482d6d25945fd6f087ae
Akila Premachandra <apremach@amd.com>
2020-07-31 22:55:54 +0000

    Fix path

--- .jenkins/staticlibrary.groovy
+++ .jenkins/staticlibrary.groovy
@@ -17,7 +17,7 @@ def runCI =
 
     prj.paths.build_command = './install.sh -c --static'
     prj.compiler.compiler_name = 'hipcc'
-    prj.compiler.compiler_path = '/opt/rocm/bin'
+    prj.compiler.compiler_path = '/opt/rocm/bin/hipcc'
     prj.libraryDependencies = ['rocSPARSE-internal', 'rocPRIM']
 
     // Define test architectures, optional rocm version argument is available
