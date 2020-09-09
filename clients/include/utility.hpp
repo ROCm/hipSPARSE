a8040901ea42383e6dbdc60cf9dac9b5c586cc6d
James Sandham <33790278+jsandham@users.noreply.github.com>
2020-09-08 17:03:06 -0700

    Prune dense2csr by percentage (#134)
    * implementing pruneDense2csrByPercentage in hipsparse
    
    * clang formatting
    
    * formatting
    
    * formatting
    
    * removing iostream include
    
    Co-authored-by: jsandham <james.sandham@amd.com>
    
    [ROCm/hipSPARSE commit: 61be3d1c1f448459b499fc0ab179217f38c8901d]

--- clients/include/utility.hpp
+++ clients/include/utility.hpp
@@ -1083,6 +1083,38 @@ void host_prune_dense2csr(int                   m,
     }
 }
 
+template <typename T>
+void host_prune_dense2csr_by_percentage(int                   m,
+                                        int                   n,
+                                        const std::vector<T>& A,
+                                        int                   lda,
+                                        hipsparseIndexBase_t  base,
+                                        T                     percentage,
+                                        int&                  nnz,
+                                        std::vector<T>&       csr_val,
+                                        std::vector<int>&     csr_row_ptr,
+                                        std::vector<int>&     csr_col_ind)
+{
+    int nnz_A = m * n;
+    int pos   = std::ceil(nnz_A * (percentage / 100)) - 1;
+    pos       = std::min(pos, nnz_A - 1);
+    pos       = std::max(pos, 0);
+
+    std::vector<T> sorted_A(m * n);
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            sorted_A[m * i + j] = std::abs(A[lda * i + j]);
+        }
+    }
+
+    std::sort(sorted_A.begin(), sorted_A.end());
+
+    T threshold = sorted_A[pos];
+    host_prune_dense2csr<T>(m, n, A, lda, base, threshold, nnz, csr_val, csr_row_ptr, csr_col_ind);
+}
+
 template <hipsparseDirection_t DIRA, typename T>
 void host_csx2dense(int                  m,
                     int                  n,
@@ -4049,7 +4081,8 @@ public:
     double alphai    = 0.0;
     double beta      = 0.0;
     double betai     = 0.0;
-    double threshold = 0.0f;
+    double threshold = 0.0;
+    double percentage = 0.0;
 
     hipsparseOperation_t    transA    = HIPSPARSE_OPERATION_NON_TRANSPOSE;
     hipsparseOperation_t    transB    = HIPSPARSE_OPERATION_NON_TRANSPOSE;
@@ -4091,6 +4124,7 @@ public:
         this->beta      = rhs.beta;
         this->betai     = rhs.betai;
         this->threshold = rhs.threshold;
+	this->percentage = rhs.percentage;
 
         this->transA    = rhs.transA;
         this->transB    = rhs.transB;
