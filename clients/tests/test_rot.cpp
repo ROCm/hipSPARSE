400f4da0807685af4f030d1697933097ce3e15d9
Nico <31079890+ntrost57@users.noreply.github.com>
2020-12-14 09:26:55 +0100

    generic rot (#156)
    * generic rot
    
    * bump version
    
    [ROCm/hipSPARSE commit: b4c17e1738006c347583acce581df7277f319185]

---
+++ clients/tests/test_rot.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************
+ * Copyright (c) 2020 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * ************************************************************************ */
+
+#include "testing_rot.hpp"
+
+#include <gtest/gtest.h>
+#include <hipsparse.h>
+
+TEST(rot_bad_arg, rot_float)
+{
+    testing_rot_bad_arg();
+}
+
+TEST(rot, rot_i32_float)
+{
+    hipsparseStatus_t status = testing_rot<int32_t, float>();
+    EXPECT_EQ(status, HIPSPARSE_STATUS_SUCCESS);
+}
+
+TEST(rot, rot_i64_double)
+{
+    hipsparseStatus_t status = testing_rot<int64_t, double>();
+    EXPECT_EQ(status, HIPSPARSE_STATUS_SUCCESS);
+}
+
+TEST(rot, rot_i32_hipFloatComplex)
+{
+    hipsparseStatus_t status = testing_rot<int32_t, hipComplex>();
+    EXPECT_EQ(status, HIPSPARSE_STATUS_SUCCESS);
+}
+
+TEST(rot, rot_i64_hipDoubleComplex)
+{
+    hipsparseStatus_t status = testing_rot<int64_t, hipDoubleComplex>();
+    EXPECT_EQ(status, HIPSPARSE_STATUS_SUCCESS);
+}
