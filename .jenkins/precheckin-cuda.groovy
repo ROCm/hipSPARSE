e83483b126c9e1a95d787c4204dfb9e3db3b5cbe
Eiden Yoshida <47196116+eidenyoshida@users.noreply.github.com>
2021-02-05 13:54:57 -0700

    Add cuda jenkinsfile (#166)

---
+++ .jenkins/precheckin-cuda.groovy
@@ -0,0 +1,60 @@
+#!/usr/bin/env groovy
+// This shared library is available at https://github.com/ROCmSoftwarePlatform/rocJENKINS/
+@Library('rocJenkins@pong') _
+
+// This file is for internal AMD use.
+// If you are interested in running your own Jenkins, please raise a github issue for assistance.
+
+import com.amd.project.*
+import com.amd.docker.*
+import java.nio.file.Path;
+
+def runCI = 
+{
+    nodeDetails, jobName->
+
+    def prj = new rocProject('hipSPARSE', 'PreCheckin-Cuda')
+
+    prj.paths.build_command = './install.sh -c --cuda'
+    prj.compiler.compiler_name = 'c++'
+    prj.compiler.compiler_path = 'c++'
+
+    // Define test architectures, optional rocm version argument is available
+    def nodes = new dockerNodes(nodeDetails, jobName, prj)
+
+    boolean formatCheck = false
+
+    def commonGroovy
+
+    def compileCommand =
+    {
+        platform, project->
+
+        commonGroovy = load "${project.paths.project_src_prefix}/.jenkins/common.groovy"
+        commonGroovy.runCompileCommand(platform, project)
+    }
+
+    def packageCommand =
+    {
+        platform, project->
+        
+        commonGroovy.runPackageCommand(platform, project)
+    }
+
+    //Temporarily disable testing
+    buildProject(prj, formatCheck, nodes.dockerArray, compileCommand, null, packageCommand)
+}
+
+ci: { 
+    String urlJobName = auxiliary.getTopJobName(env.BUILD_URL)
+
+    def jobNameList = [:]
+
+    // For url job names that are not listed by the jobNameList i.e. compute-rocm-dkms-no-npi-1901
+    if(!jobNameList.keySet().contains(urlJobName))
+    {
+        properties(auxiliary.addCommonProperties([pipelineTriggers([cron('0 1 * * 6')])]))
+        runCI(['ubuntu20-cuda11':['anycuda']], urlJobName)
+    }
+}
+
