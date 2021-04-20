9166676529fcc657b437574d934834f73e2329ca
jsandham <james.sandham@amd.com>
2021-04-20 19:59:53 +0000

    fix merge conflict

--- library/src/nvcc_detail/hipsparse.cpp
+++ library/src/nvcc_detail/hipsparse.cpp
@@ -10743,6 +10743,34 @@ hipsparseStatus_t hipsparseZgtsv2_nopivot(hipsparseHandle_t       handle,
 }
 
 #if(CUDART_VERSION >= 11020)
+hipsparseStatus_t hipsparseSDDMM(hipsparseHandle_t            handle,
+                                 hipsparseOperation_t         opA,
+                                 hipsparseOperation_t         opB,
+                                 const void*                  alpha,
+                                 const hipsparseDnMatDescr_t  matA,
+                                 const hipsparseDnMatDescr_t  matB,
+                                 const void*                  beta,
+                                 hipsparseSpMatDescr_t        matC,
+                                 hipDataType                  computeType,
+                                 hipsparseSDDMMAlg_t          alg,
+                                 void*                        tempBuffer)
+{
+  return hipCUSPARSEStatusToHIPStatus(cusparseSDDMM((cusparseHandle_t)handle,
+						    hipOperationToCudaOperation(opA),
+						    hipOperationToCudaOperation(opB),
+						    alpha,
+						    (const cusparseDnMatDescr_t)matA,
+						    (const cusparseDnMatDescr_t)matB,
+						    beta,
+						    (cusparseSpMatDescr_t)matC,
+						    hipDataTypeToCudaDataType(computeType),
+						    hipSDDMMAlgToCudaSDDMMAlg(alg),
+						    tempBuffer));
+}
+#endif
+
+
+#if(CUDART_VERSION >= 11020)
 hipsparseStatus_t hipsparseSDDMM_bufferSize(hipsparseHandle_t           handle,
                                             hipsparseOperation_t        opA,
                                             hipsparseOperation_t        opB,
