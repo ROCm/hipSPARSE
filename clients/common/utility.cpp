84c156c39b9b3f0d144e771b93659888c1897453
Saad Rahim <44449863+saadrahim@users.noreply.github.com>
2021-11-09 14:53:46 -0700

    Removing redundant reference (#230)
    [ROCm/hipSPARSE commit: fe41e6ec7d053f4c56cc61cb8a369168437bbe0f]

--- clients/common/utility.cpp
+++ clients/common/utility.cpp
@@ -1,5 +1,5 @@
 /* ************************************************************************
- * Copyright (c) 2018-2019 Advanced Micro Devices, Inc.
+ * Copyright (c) 2018-2021 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -60,9 +60,6 @@ namespace std
 #include <cmath>
 #include <cstdlib>
 
-//
-// https://en.cppreference.com/w/User:D41D8CD98F/feature_testing_macros
-//
 #ifdef __cpp_lib_filesystem
 #include <filesystem>
 #else
