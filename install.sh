79c2cf45e333552b202c86bf592b17de9669e448
Ranjith Ramakrishnan <Ranjith.Ramakrishnan@amd.com>
2022-11-16 09:01:43 -0800

    Remove rpath from ROCm binaries
    [ROCm/hipSPARSE commit: da93a3b2965271ad3a31b24cf74a070e0ca1f072]

--- install.sh
+++ install.sh
@@ -462,10 +462,10 @@ pushd .
   if [[ "${build_relocatable}" == true ]]; then
     CXX=${compiler} ${cmake_executable} ${cmake_common_options} ${cmake_client_options} \
       -DCMAKE_INSTALL_PREFIX="${install_prefix}" \
-      -DCMAKE_SHARED_LINKER_FLAGS="" \
       -DCMAKE_PREFIX_PATH="${rocm_path} ${rocm_path}/hcc ${rocm_path}/hip" \
       -DCMAKE_MODULE_PATH="${rocm_path}/lib/cmake/hip ${rocm_path}/hip/cmake" \
-      -DCMAKE_EXE_LINKER_FLAGS=" -Wl,--disable-new-dtags -Wl,--rpath,${rocm_path}/lib:${rocm_path}/lib64 ${cmake_build_static_options}" \
+      -DCMAKE_EXE_LINKER_FLAGS=" ${cmake_build_static_options}" \
+      -DCMAKE_SKIP_INSTALL_RPATH=TRUE \
       -DROCM_DISABLE_LDCONFIG=ON \
       -DROCM_PATH="${rocm_path}" ../..
   else
