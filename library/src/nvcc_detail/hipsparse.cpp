6d132d5ed2c4e72afa2ba578b293b75c92d6ddc8
cgy-decidueye <angonzal@amd.com>
2023-03-07 13:50:41 -0700

    fixes
    [ROCm/hipSPARSE commit: 791a73051fb38b8b98d7c69f2e984764d0989a96]

--- library/src/nvcc_detail/hipsparse.cpp
+++ library/src/nvcc_detail/hipsparse.cpp
@@ -13186,8 +13186,7 @@ hipsparseStatus_t hipsparseSpSV_solve(hipsparseHandle_t handle,
                                       const hipsparseDnVecDescr_t y,
                                       hipDataType computeType,
                                       hipsparseSpSVAlg_t alg,
-                                      hipsparseSpSVDescr_t spsvDescr,
-                                      void* externalBuffer)
+                                      hipsparseSpSVDescr_t spsvDescr)
 {
     return hipCUSPARSEStatusToHIPStatus(cusparseSpSV_solve((cusparseHandle_t)handle,
                                                            hipOperationToCudaOperation(opA),
