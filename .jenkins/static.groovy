c239e99a65812d47c32c573d7ca2fca690bd21e3
Sam Wu <sam.wu2@amd.com>
2024-02-27 11:30:49 -0700

    Re-enable static library CI

--- .jenkins/static.groovy
+++ .jenkins/static.groovy
@@ -13,11 +13,11 @@ def runCI =
 {
     nodeDetails, jobName->
 
-    def prj = new rocProject('hipSPARSE', 'Static Library PreCheckin')
+    def prj = new rocProject('hipSPARSE', 'static')
 
     prj.paths.build_command = './install.sh -c --static'
-    prj.compiler.compiler_name = 'hipcc'
-    prj.compiler.compiler_path = '/opt/rocm/bin/hipcc'
+    prj.compiler.compiler_name = 'c++'
+    prj.compiler.compiler_path = 'c++'
     prj.libraryDependencies = ['rocBLAS', 'rocSPARSE', 'rocPRIM']
     prj.defaults.ccache = false
 
@@ -33,7 +33,7 @@ def runCI =
         platform, project->
 
         commonGroovy = load "${project.paths.project_src_prefix}/.jenkins/common.groovy"
-        commonGroovy.runCompileCommand(platform, project,true)
+        commonGroovy.runCompileCommand(platform, project, true)
     }
 
     def testCommand =
@@ -93,4 +93,3 @@ ci: {
         runCI([ubuntu18:['gfx906']], urlJobName)       
     }
 }
-
