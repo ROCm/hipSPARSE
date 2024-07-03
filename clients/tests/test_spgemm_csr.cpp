303e6388eac66cc978af88f47e50d1c16abccbc6
jsandham <james.sandham@amd.com>
2024-07-03 22:44:53 +0000

    Fix compilation failure

--- clients/tests/test_spgemm_csr.cpp
+++ clients/tests/test_spgemm_csr.cpp
@@ -38,16 +38,16 @@ int spgemm_csr_K_range[] = {84, 649, 5148};
 std::vector<double> spgemm_csr_alpha_range = {2.0};
 
 #if(!defined(CUDART_VERSION))
-hipsparseIndexBase_t spgemmreuse_csr_idxbaseA_range[]
+hipsparseIndexBase_t spgemm_csr_idxbaseA_range[]
     = {HIPSPARSE_INDEX_BASE_ZERO, HIPSPARSE_INDEX_BASE_ONE};
-hipsparseIndexBase_t spgemmreuse_csr_idxbaseB_range[]
+hipsparseIndexBase_t spgemm_csr_idxbaseB_range[]
     = {HIPSPARSE_INDEX_BASE_ZERO, HIPSPARSE_INDEX_BASE_ONE};
-hipsparseIndexBase_t spgemmreuse_csr_idxbaseC_range[]
+hipsparseIndexBase_t spgemm_csr_idxbaseC_range[]
     = {HIPSPARSE_INDEX_BASE_ZERO, HIPSPARSE_INDEX_BASE_ONE};
 #else // All matrices must use the same base index in cusparse
-hipsparseIndexBase_t spgemmreuse_csr_idxbaseA_range[] = {HIPSPARSE_INDEX_BASE_ZERO};
-hipsparseIndexBase_t spgemmreuse_csr_idxbaseB_range[] = {HIPSPARSE_INDEX_BASE_ZERO};
-hipsparseIndexBase_t spgemmreuse_csr_idxbaseC_range[] = {HIPSPARSE_INDEX_BASE_ZERO};
+hipsparseIndexBase_t spgemm_csr_idxbaseA_range[] = {HIPSPARSE_INDEX_BASE_ZERO};
+hipsparseIndexBase_t spgemm_csr_idxbaseB_range[] = {HIPSPARSE_INDEX_BASE_ZERO};
+hipsparseIndexBase_t spgemm_csr_idxbaseC_range[] = {HIPSPARSE_INDEX_BASE_ZERO};
 #endif
 
 std::string spgemm_csr_bin[] = {"nos1.bin",
