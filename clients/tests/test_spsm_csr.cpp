6c1d568b9c498e8c649b31b6102d780103d8fa3d
jsandham <james.sandham@amd.com>
2024-07-03 22:13:56 +0000

    Use same base index for all matrices when testing spgemm with cusparse backend

--- clients/tests/test_spsm_csr.cpp
+++ clients/tests/test_spsm_csr.cpp
@@ -62,7 +62,7 @@ hipsparseDiagType_t spsm_csr_diag_type_range[] = {HIPSPARSE_DIAG_TYPE_NON_UNIT};
 hipsparseFillMode_t spsm_csr_fill_mode_range[]
     = {HIPSPARSE_FILL_MODE_LOWER, HIPSPARSE_FILL_MODE_UPPER};
 
-std::string spsm_csr_bin[] = {"nos1.bin", "nos4.bin", "nos5.bin", "scircuit.bin"};
+std::string spsm_csr_bin[] = {"nos1.bin", "nos3.bin", "nos5.bin", "scircuit.bin"};
 
 class parameterized_spsm_csr : public testing::TestWithParam<spsm_csr_tuple>
 {
