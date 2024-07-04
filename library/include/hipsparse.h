9871a53fa5484bed43108924ac7f397c35be6b7d
jsandham <james.sandham@amd.com>
2024-07-04 19:34:43 +0000

    Fix documentation typos

--- library/include/hipsparse.h
+++ library/include/hipsparse.h
@@ -12030,11 +12030,11 @@ hipsparseStatus_t hipsparseDnMatSetStridedBatch(hipsparseDnMatDescr_t dnMatDescr
 *  \f]
 *
 *  \code{.c}
-*      for(i = 0; i < size - 1; ++i)
+*      for(i = 0; i < size; ++i)
 *      {
 *          y[i] = beta * y[i]
 *      }
-*      for(i = 0; i < nnz - 1; ++i)
+*      for(i = 0; i < nnz; ++i)
 *      {
 *          y[x_ind[i]] = alpha * x_val[i]
 *      }
