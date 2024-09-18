e6ff574c8ccb06c56b2e1f9182884f3703ce5730
Sam Wu <22262939+samjwu@users.noreply.github.com>
2024-09-18 10:09:54 -0600

    Define propertyList

--- .jenkins/precheckin-cuda.groovy
+++ .jenkins/precheckin-cuda.groovy
@@ -57,7 +57,8 @@ def runCI =
 ci: { 
     String urlJobName = auxiliary.getTopJobName(env.BUILD_URL)
 
-    def propertyList = auxiliary.appendPropertyList(propertyList)
+    def propertyList = [:]
+    propertyList = auxiliary.appendPropertyList(propertyList)
 
     def jobNameList = [:]
     jobNameList = auxiliary.appendJobNameList(jobNameList, 'hipSPARSE')
