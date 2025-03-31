ed27f09cda2dae982f15cf9ebcc7f4986806ad40
jsandham <jsandham@amd.com>
2025-03-31 18:28:04 -0400

    Fix incorrect use of runCompileCOmmand in codecov groovy file

--- .jenkins/codecov.groovy
+++ .jenkins/codecov.groovy
@@ -33,7 +33,7 @@ def runCI =
         platform, project->
 
         commonGroovy = load "${project.paths.project_src_prefix}/.jenkins/common.groovy"
-        commonGroovy.runCompileCommand(platform, project, jobName)
+        commonGroovy.runCompileCommand(platform, project)
     }
 
     def testCommand =
