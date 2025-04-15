ae787a0870ac8aaeeee7d73cd83f81a8335687ff
jsandham <jsandham@amd.com>
2025-04-15 20:12:27 +0000

    Clang formatting

--- clients/tests/test_csr2csc_ex2.cpp
+++ clients/tests/test_csr2csc_ex2.cpp
@@ -106,7 +106,7 @@ Arguments setup_csr2csc_ex2_arguments(csr2csc_ex2_bin_tuple tup)
     return arg;
 }
 
-#if (!defined(CUDART_VERSION) || CUDART_VERSION >= 10010)
+#if(!defined(CUDART_VERSION) || CUDART_VERSION >= 10010)
 TEST(csr2csc_ex2_bad_arg, csr2csc_ex2)
 {
     testing_csr2csc_ex2_bad_arg<float>();
