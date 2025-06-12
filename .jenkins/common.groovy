58e6ad0bbba72c21ac26226495e519c8223f4d00
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-06-12 13:25:33 -0600

    Upload codecov report to codecov.io

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -42,14 +42,24 @@ def runTestCommand (platform, project, gfilter)
 
 def runCoverageCommand (platform, project, gfilter, String dirmode = "release")
 {
-    def command = """#!/usr/bin/env bash
-                set -x
-                cd ${project.paths.project_build_prefix}/build/${dirmode}
-                export LD_LIBRARY_PATH=/opt/rocm/lib/
-                GTEST_LISTENER=NO_PASS_LINE_IN_LOG make coverage_cleanup coverage GTEST_FILTER=${gfilter}-*known_bug*
-            """
+    String commitSha
+    String repoUrl
+    (commitSha, repoUrl) = util.getGitHubCommitInformation(project.paths.project_src_prefix)
 
-    platform.runCommand(this, command)
+    withCredentials([string(credentialsId: "mathlibs-codecov-token-hipsparse", variable: 'CODECOV_TOKEN')])
+    {
+        def command = """#!/usr/bin/env bash
+                    set -x
+                    cd ${project.paths.project_build_prefix}/build/${dirmode}
+                    export LD_LIBRARY_PATH=/opt/rocm/lib/
+                    GTEST_LISTENER=NO_PASS_LINE_IN_LOG make coverage_cleanup coverage GTEST_FILTER=${gfilter}-*known_bug*
+                    curl -Os https://uploader.codecov.io/latest/linux/codecov
+                    chmod +x codecov
+                    ./codecov -v -U \$http_proxy -t ${CODECOV_TOKEN} --file lcoverage/main_coverage.info --name hipSPARSE --sha ${commitSha}
+                """
+
+        platform.runCommand(this, command)
+    }
 
     publishHTML([allowMissing: false,
                 alwaysLinkToLastBuild: false,
