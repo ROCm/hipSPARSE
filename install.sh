12b4ce62041aa9b810b8b2a53c4d98f00912d1c3
James Sandham <33790278+jsandham@users.noreply.github.com>
2026-05-14 19:57:41 +0000

    [rocm-libraries] ROCm/rocm-libraries#7051 (commit 02c3023)
    [hipsparse] Build hipsparse-bench executable as part of
     clients (#7051)
    
    ## Motivation
    
    The building of the hipsparse-bench execatuble was disabled in the
    recent hipsparse build refactor PR:
    https://github.com/ROCm/rocm-libraries/pull/2714. This PR adds it back
    so that the install script command:
    
    `./install.sh -dci`
    
    builds the test client AND the benchmark executable.
    
    https://amd-hub.atlassian.net/browse/AISPARSE-546

--- install.sh
+++ install.sh
@@ -503,7 +503,7 @@ pushd .
 
   # clients
   if [[ "${build_clients}" == true ]]; then
-    cmake_client_options="${cmake_client_options} -DBUILD_CLIENTS_SAMPLES=ON -DBUILD_CLIENTS_TESTS=ON"
+    cmake_client_options="${cmake_client_options} -DBUILD_CLIENTS_SAMPLES=ON -DBUILD_CLIENTS_TESTS=ON -DBUILD_CLIENTS_BENCHMARKS=ON"
     #
     # Add matrices_dir if exists.
     #
@@ -514,7 +514,7 @@ pushd .
 
   # clients only
   if [[ "${build_clients_only}" == true ]]; then
-    cmake_client_options="${cmake_client_options} -DBUILD_CLIENTS_ONLY=ON -DBUILD_CLIENTS_SAMPLES=ON -DBUILD_CLIENTS_TESTS=ON"
+    cmake_client_options="${cmake_client_options} -DBUILD_CLIENTS_ONLY=ON -DBUILD_CLIENTS_SAMPLES=ON -DBUILD_CLIENTS_TESTS=ON -DBUILD_CLIENTS_BENCHMARKS=ON"
     #
     # Add matrices_dir if exists.
     #
